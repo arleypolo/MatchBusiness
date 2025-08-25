3c6d94f83ea867797bbe28c322aa06dd8cd3c2de
pablojimora <pablojimora@gmail.com>
2025-08-25 09:36:50 -0500

    delete clg

--- BACK/Models/companies.models.js
+++ BACK/Models/companies.models.js
@@ -22,20 +22,16 @@ const getCompanyByIdModel = async (id) => {
 const postCompaniesModel = async ({ id, company_name, sector, description, password}) => {
     const company = await db.connect();
     try {
-        await company.query('BEGIN');
-        console.log('Inicio transaccion');
-
+        await company.query('BEGIN')
 
         const queryUsers = `INSERT INTO users (id_user, password, id_role, is_active) VALUES ($1, $2, $3, $4) RETURNING *`;
         await company.query(queryUsers, [id, password, 2, true]);
-        console.log('Primera query')
 
         const queryCompanies = `INSERT INTO companies (id_company, company_name, sector, description) VALUES ($1, $2, $3, $4) RETURNING *`;
         const companyResult = await company.query(queryCompanies, [id, company_name, sector, description]);
-        console.log('SEgunda query')
+
 
         await company.query('COMMIT');
-        console.log('COmmit')
         return companyResult.rows[0];
     } catch (error) {
         await company.query('ROLLBACK');
