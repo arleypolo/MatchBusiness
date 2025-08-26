24685236f8b9b01bf6ce50028c9edadba823917c
pablojimora <pablojimora@gmail.com>
2025-08-26 09:57:02 -0500

    POST matches

--- BACK/Controllers/matchesController.js
+++ BACK/Controllers/matchesController.js
@@ -10,3 +10,30 @@ export const getMyMatches = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
+
+export const createMatch = async (req, res) => {
+    try {
+        const { id_idea, match_date } = req.body;
+
+        if (!id_idea) {
+            return res.status(400).json({ error: 'The match must be associated with an idea' });
+        }
+        if (!match_date) {
+            return res.status(400).json({ error: 'Match date must not be null' });
+        }
+
+        const newMatch = await matchesModel.postMatchesModel({ id_idea, match_date });
+
+        res.status(201).json({
+            message: "Match created successfully",
+            match: newMatch
+        });
+
+    } catch (error) {
+        if (error.code === '23503') { // clave foránea inválida
+            return res.status(400).json({ error: 'The idea is not associated with an active coder or company' });
+        }
+        console.error("Error creating match:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
